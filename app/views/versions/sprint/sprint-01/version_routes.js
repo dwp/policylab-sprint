e52eab61545a171e45771f792448ea2a96110e92
Paul Smith <mail@airfront.co.uk>
2016-09-29 13:32:42 +0100

    removes auth for research

--- app/views/versions/sprint/sprint-01/version_routes.js
+++ app/views/versions/sprint/sprint-01/version_routes.js
@@ -8,7 +8,41 @@ module.exports = function(router, config) {
   router.all(config.routes.step, function(req,res, next){
     
     var requestedPage = req.params.step,
-    postData = req.body || {};
+    postData = req.body ? req.body : {},
+    sessionData = req.session.data ? req.session.data : {},
+    queryData = url.parse(req.url,true).query;
+    
+    var planData = [
+      {
+        wish: 'Name of wish',
+        outcome: 'outcome text',
+        obstacle: ['name of obstacle'],
+        actions: [
+          {
+            task: 'Task title here 1',
+            date_day: '01',
+            date_month: '01',
+            date_year: '2017'
+          },
+          {
+            task: 'Task title here 2',
+            date_day: '01',
+            date_month: '02',
+            date_year: '2017'
+          },
+          {
+            task: 'Task title here 3',
+            date_day: '01',
+            date_month: '03',
+            date_year: '2017'
+          },
+        ]
+      }
+    ];
+    
+    if (!sessionData.actions) {
+      sessionData.actions = [];
+    }
     
     // create a deep object of the setting the current prototype's nav data
     _.set(res.locals, 'prototype.current.sitemap', sitemap);
@@ -16,11 +50,11 @@ module.exports = function(router, config) {
     switch(requestedPage) {
       
       // change requested page name to the page that was requested
-      case 'request_page_name_here': 
+      case 'store_action':
         
-        // do some logic stuff here and then typically return a render or redirect
-      
-        return res.redirect('index');
+        sessionData.actions.push(postData);
+        
+        return res.redirect('plan');
         
       break;
       
